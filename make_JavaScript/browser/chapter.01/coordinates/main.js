2baff21516fbb47da2f7cfd2dcc489979b93c97a
mina <yegrinaaa@gmail.com>
2021-03-20 01:07:40 +0900

    0320 |[Refactoring] mousemove Event Target

--- make_JavaScript/browser/chapter.01/coordinates/main.js
+++ make_JavaScript/browser/chapter.01/coordinates/main.js
@@ -1,15 +1,33 @@
-const target = document.querySelector(".target");
-const txt = document.querySelector(".txt");
-target.style.position = "absolute";
-target.style.top = "0";
-target.style.left = "0";
+// const target = document.querySelector(".target");
+// const txt = document.querySelector(".txt");
+// target.style.position = "absolute";
+// target.style.top = "0";
+// target.style.left = "0";
 
-window.addEventListener("mousemove", (e) => {
+// window.addEventListener("mousemove", (e) => {
+//   x = e.clientX;
+//   y = e.clientY;
+//   target.style.left = `${x}px`;
+//   target.style.top = `${y}px`;
+//   console.log(target.style.top);
+//   txt.innerHTML = `${target.style.left} <br>${target.style.top}`;
+// });
+// addEventListener();
+
+const vertical = document.querySelector(".vertical");
+const horizontal = document.querySelector(".horizontal");
+const target = document.querySelector(".target");
+const tag = document.querySelector(".tag");
+
+document.addEventListener("mousemove", (e) => {
   x = e.clientX;
   y = e.clientY;
+
+  vertical.style.left = `${x}px`;
+  horizontal.style.top = `${y}px`;
   target.style.left = `${x}px`;
   target.style.top = `${y}px`;
-  console.log(target.style.top);
-  txt.innerHTML = `${target.style.left} <br>${target.style.top}`;
+  tag.style.left = `${x}px`;
+  tag.style.top = `${y}px`;
+  tag.innerHTML = `${x}px, ${y}px`;
 });
-addEventListener();
