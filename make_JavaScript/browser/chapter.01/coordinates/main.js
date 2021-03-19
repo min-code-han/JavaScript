49aff1798c8aa854766f02002cda9417a80a0e78
mina <yegrinaaa@gmail.com>
2021-03-20 00:11:20 +0900

    0320 | px값 추가

--- make_JavaScript/browser/chapter.01/coordinates/main.js
+++ make_JavaScript/browser/chapter.01/coordinates/main.js
@@ -1,4 +1,5 @@
 const target = document.querySelector(".target");
+const txt = document.querySelector(".txt");
 target.style.position = "absolute";
 target.style.top = "0";
 target.style.left = "0";
@@ -6,8 +7,9 @@ target.style.left = "0";
 window.addEventListener("mousemove", (e) => {
   x = e.clientX;
   y = e.clientY;
-  target.style.left = `${x - 50}px`;
-  target.style.top = `${y - 50}px`;
+  target.style.left = `${x}px`;
+  target.style.top = `${y}px`;
   console.log(target.style.top);
+  txt.innerHTML = `${target.style.left} <br>${target.style.top}`;
 });
 addEventListener();
