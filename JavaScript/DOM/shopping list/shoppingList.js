230c603fdff04437f8c3edc110d1ec30c231b083
mina <yegrinaaa@gmail.com>
2021-03-26 17:22:25 +0900

    0326 | Shoppong List 완성

--- JavaScript/DOM/shopping list/shoppingList.js
+++ JavaScript/DOM/shopping list/shoppingList.js
@@ -1,20 +1,66 @@
-const memo = document.querySelector('ul');
-const add = document.querySelector('.add');
-const deleteBtn = document.querySelector('.delete');
+const items = document.querySelector('.items');
+const input = document.querySelector('.footer_input');
+const addBtn = document.querySelector('.footer_button');
 
-add.addEventListener('click', (e) => {
-  const list = document.createElement('li');
-  const value = document.querySelector('input').value;
+function onAdd() {
+  // 1. 사용자가 입력한 txt 받아오기.
+  const text = input.value;
+  if (text === '') {
+    input.focus();
+    return;
+  }
 
-  list.innerHTML = `
-    <span>${value}</span>
-    <button class="delete">remove</button>
-  `;
+  // 2. 새로운 아이템 만들기. (txt + 삭제버튼)
+  const item = createItem(text);
 
-  memo.appendChild(list);
+  // 3. items 컨테이너 안에 새로만든 아이템 추가하기.
+  items.appendChild(item);
+
+  // 4. 새로추가된 아이템으로 스크롤 이동
+  item.scrollIntoView({ block: 'center' });
+
+  // 5. input 초기화 하기.
+  input.value = '';
+  input.focus();
+}
+
+function createItem(text) {
+  const itemRow = document.createElement('li');
+  itemRow.setAttribute('class', 'item_row');
+
+  const item = document.createElement('div');
+  item.setAttribute('class', 'item');
+
+  const name = document.createElement('span');
+  name.setAttribute('class', 'item_name');
+  name.innerText = text;
+  console.log(text);
+
+  const deleteBtn = document.createElement('button');
+  deleteBtn.setAttribute('class', 'item_delete');
+  deleteBtn.innerHTML = '<i class="far fa-trash-alt"></i>';
+  deleteBtn.addEventListener('click', () => {
+    items.removeChild(itemRow);
+  });
+
+  const itemDivider = document.createElement('div');
+  itemDivider.setAttribute('class', 'item_divider');
+
+  item.appendChild(name);
+  item.appendChild(deleteBtn);
+
+  itemRow.appendChild(item);
+  itemRow.appendChild(itemDivider);
+  return itemRow;
+}
+
+addBtn.addEventListener('click', () => {
+  onAdd();
 });
 
-deleteBtn.addEventListener('click', (e) => {
-  memo.removeChild(list);
-  console.log(a);
+input.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    onAdd();
+  }
+  el;
 });
