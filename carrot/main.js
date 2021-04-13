e1a52198169f88a753e9191bff1e377665fda12e
mina <yegrinaaa@gmail.com>
2021-04-14 00:09:11 +0900

    당근게임 당근/벌레 랜덤배치 로직 완료

---
+++ carrot/main.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const field = document.querySelector('.game_field');
+const fieldRect = field.getBoundingClientRect(); //field의 위치값 받아올수 있음
+
+function initGame() {
+  //들어갈 요소들 생성해서 field에 추가해주기
+  console.log(fieldRect);
+  addItem('carrot', 5, 'img/carrot.png');
+  addItem('bug', 5, 'img/bug.png');
+}
+
+function addItem(className, count, imgPath) {
+  const x1 = 0;
+  const y1 = 0;
+  const x2 = fieldRect.width;
+  const y2 = fieldRect.height;
+  for (let i = 0; i < count; i++) {
+    const item = document.createElement('img');
+    item.setAttribute('class', className);
+    item.setAttribute('src', imgPath);
+    item.style.position = 'absolute';
+    const x = randomNumber(x1, x2);
+    const y = randomNumber(y1, y2);
+    item.style.left = `${x}px`;
+    item.style.top = `${y}px`;
+    field.appendChild(item);
+  }
+}
+
+function randomNumber(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+initGame();
