a28055d2cf102377d7d3c4e2e663e8a26faf7383
mina <yegrinaaa@gmail.com>
2021-04-23 16:14:52 +0900

    [REFACTOR] Carrot Game _ Field Class 생성

---
+++ carrot/src/field.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const carrotSound = new Audio('./sound/carrot_pull.mp3');
+const CARROT_SIZE = 80;
+
+export default class Field {
+  constructor(carrotCount, bugCount) {
+    this.carrotCount = carrotCount;
+    this.bugCount = bugCount;
+    this.field = document.querySelector('.game_field');
+    this.fieldRect = this.field.getBoundingClientRect(); //field의 위치값 받아올수 있음
+    this.field.addEventListener('click', this.onClick);
+  }
+
+  init() {
+    this.field.innerHTML = '';
+    this._addItem('carrot', this.carrotCount, 'img/carrot.png');
+    this._addItem('bug', this.bugCount, 'img/bug.png');
+  }
+
+  setClickListener(onItemClick) {
+    this.onItemClick = onItemClick;
+  }
+
+  _addItem(className, count, imgPath) {
+    const x1 = 0;
+    const y1 = 0;
+    const x2 = this.fieldRect.width - CARROT_SIZE;
+    const y2 = this.fieldRect.height - CARROT_SIZE;
+    for (let i = 0; i < count; i++) {
+      const item = document.createElement('img');
+      item.setAttribute('class', className);
+      item.setAttribute('src', imgPath);
+      item.style.position = 'absolute';
+      const x = randomNumber(x1, x2);
+      const y = randomNumber(y1, y2);
+      item.style.left = `${x}px`;
+      item.style.top = `${y}px`;
+      this.field.appendChild(item);
+    }
+  }
+
+  onClick(e) {
+    const target = e.target;
+    if (target.matches('.carrot')) {
+      target.remove();
+      this.onItemClick && this.onItemClick('carrot');
+      playSound(carrotSound);
+    } else if (target.matches('.bug')) {
+      this.onItemClick && this.onItemClick('bug');
+    }
+  }
+}
+
+function playSound(sound) {
+  sound.currentTime = 0;
+  sound.play();
+}
+
+function randomNumber(min, max) {
+  return Math.random() * (max - min) + min;
+}
